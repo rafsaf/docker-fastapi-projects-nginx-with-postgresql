404653c8fa4fdf5a08a6c5dc8a1001fb06d749a6
rafsaf <rafal.safin12@gmail.com>
2021-04-16 14:42:12 +0200

    basic template

---
+++ app/db/migrations/versions/66bd3c883891_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: 66bd3c883891
+Revises: 
+Create Date: 2021-04-16 13:41:32.536578
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '66bd3c883891'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
