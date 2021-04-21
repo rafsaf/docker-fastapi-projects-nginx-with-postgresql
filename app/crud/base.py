b8b94547920e56400ac78907280ad4ebf5b62aa4
Rafał Safin <rafal.safin12@gmail.com>
2021-04-21 10:03:49 +0200

    tortoise orm

--- app/crud/base.py
+++ app/crud/base.py
@@ -1,11 +1,9 @@
 from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union
-
+from tortoise.models import Model
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from sqlalchemy.orm import Session
-from app.db.base_class import Base
 
-ModelType = TypeVar("ModelType", bound=Base)
+ModelType = TypeVar("ModelType", bound=Model)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
 
@@ -20,28 +18,21 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         """
         self.model = model
 
-    def get(self, db: Session, id: Any) -> Optional[ModelType]:
-        return db.query(self.model).filter(self.model.id == id).first()
+    async def get_one(self, id: Any) -> Optional[ModelType]:
+        query = await self.model.get(id=id)
+        return query
 
-    def get_multi(
-        self, db: Session, *, skip: int = 0, limit: int = 100
-    ) -> List[ModelType]:
-        return db.query(self.model).offset(skip).limit(limit).all()
+    async def get_multi(self, skip: int = 0, limit: int = 100) -> List[ModelType]:
+        query = await self.model.all().offset(skip).limit(100)
+        return list(query)
 
-    def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
+    async def create_one(self, obj_in: CreateSchemaType) -> ModelType:
         obj_in_data = jsonable_encoder(obj_in)
-        db_obj = self.model(**obj_in_data)  # type: ignore
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
+        db_obj = await self.model.create(**obj_in_data)
         return db_obj
 
-    def update(
-        self,
-        db: Session,
-        *,
-        db_obj: ModelType,
-        obj_in: Union[UpdateSchemaType, Dict[str, Any]]
+    async def update_one(
+        self, db_obj: ModelType, obj_in: Union[UpdateSchemaType, Dict[str, Any]]
     ) -> ModelType:
         obj_data = jsonable_encoder(db_obj)
         if isinstance(obj_in, dict):
@@ -51,13 +42,8 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         for field in obj_data:
             if field in update_data:
                 setattr(db_obj, field, update_data[field])
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
+        await db_obj.save()
         return db_obj
 
-    def remove(self, db: Session, *, id: int) -> ModelType:
-        obj = db.query(self.model).get(id)
-        db.delete(obj)
-        db.commit()
-        return obj
+    async def remove_one(self, id: int) -> None:
+        await self.model.filter(id=id).delete()
