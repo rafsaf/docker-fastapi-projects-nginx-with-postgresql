34fa571c98f9660b53f91e35b31fd5ac2169c294
Rafał Safin <rafal.safin12@gmail.com>
2021-04-22 03:16:15 +0200

    finished test_user

--- app/tests/crud/test_user.py
+++ app/tests/crud/test_user.py
@@ -1,18 +1,25 @@
+import pytest
 from app.schemas.user import UserUpdateBySuperuser, UserUpdateMe
-from asyncio import AbstractEventLoop as EventLoop, events
+from asyncio import AbstractEventLoop as EventLoop
 from fastapi.testclient import TestClient
 from app.models import User
 from app import crud
-from app.schemas import UserCreateMe, UserCreateBySuperuser, UserPydantic
+from app.schemas import UserCreateMe, UserCreateBySuperuser
 from app.tests.utils.utils import random_email, random_lower_string
 from app.core.security import verify_password
 import logging
-import time
+
 
 logger = logging.getLogger(__name__)
 
 
-def test_user_create_me(client: TestClient, event_loop: EventLoop):
+@pytest.fixture(autouse=True)
+async def drop() -> None:
+    yield
+    await User.all().delete()
+
+
+def test_user_create_me(client: TestClient, event_loop: EventLoop, drop):
     email = random_email()
     password = random_lower_string()
     user_in = UserCreateMe(email=email, password=password)
@@ -34,23 +41,27 @@ def test_user_create_by_superuser(client: TestClient, event_loop: EventLoop):
         is_superuser=is_superuser,
         is_active=is_active,
     )
-
-    async def func():
-        user = await crud.user.create_by_superuser(user_in)
-        return user
-
-    user = event_loop.run_until_complete(func())
+    user = event_loop.run_until_complete(crud.user.create_by_superuser(user_in))
     assert user.email == email
     assert hasattr(user, "password_hash")
     assert user.is_superuser == is_superuser
     assert user.is_active == is_active
 
 
-def test_user_update_me(client: TestClient, event_loop: EventLoop):
+def test_user_authenticate(client: TestClient, event_loop: EventLoop):
     email = random_email()
     password = random_lower_string()
     user_in = UserCreateMe(email=email, password=password)
     user = event_loop.run_until_complete(crud.user.create_me(user_in))
+    assert verify_password(password, user.password_hash)
+
+
+def test_user_update_me(client: TestClient, event_loop: EventLoop):
+    email = random_email()
+    password_hash = random_lower_string()
+    user = event_loop.run_until_complete(
+        User.create(email=email, password_hash=password_hash)
+    )
 
     name = random_lower_string()
     family_name = random_lower_string()
@@ -65,9 +76,10 @@ def test_user_update_me(client: TestClient, event_loop: EventLoop):
 
 def test_user_update_by_superuser(client: TestClient, event_loop: EventLoop):
     email = random_email()
-    password = random_lower_string()
-    user_in = UserCreateMe(email=email, password=password)
-    user = event_loop.run_until_complete(crud.user.create_me(user_in))
+    password_hash = random_lower_string()
+    user = event_loop.run_until_complete(
+        User.create(email=email, password_hash=password_hash)
+    )
 
     name = random_lower_string()
     family_name = random_lower_string()
@@ -92,17 +104,40 @@ def test_user_update_by_superuser(client: TestClient, event_loop: EventLoop):
     assert updated_user.is_superuser == new_is_superuser
 
 
-def test_user_authenticate(client: TestClient, event_loop: EventLoop):
-    email = random_email()
-    password = random_lower_string()
-    user_in = UserCreateMe(email=email, password=password)
-    user = event_loop.run_until_complete(crud.user.create_me(user_in))
-    assert verify_password(password, user.password_hash)
+# Above 5 tests are slow (and should be), becouse of hash/verfiy inside takes a lot of time!
 
 
 def test_user_is_active_not(client: TestClient, event_loop: EventLoop):
     email = random_email()
-    password = random_lower_string()
-    user_in = UserCreateMe(email=email, password=password)
-    user = event_loop.run_until_complete(crud.user.create_me(user_in))
-    assert user.is_active == True
+    password_hash = random_lower_string()
+    user = event_loop.run_until_complete(
+        User.create(email=email, password_hash=password_hash)
+    )
+    assert crud.user.is_active(user) == True
+
+
+def test_user_is_active_active(client: TestClient, event_loop: EventLoop):
+    email = random_email()
+    password_hash = random_lower_string()
+    user = event_loop.run_until_complete(
+        User.create(email=email, password_hash=password_hash, is_active=False)
+    )
+    assert crud.user.is_active(user) == False
+
+
+def test_user_is_superuser_not(client: TestClient, event_loop: EventLoop):
+    email = random_email()
+    password_hash = random_lower_string()
+    user = event_loop.run_until_complete(
+        User.create(email=email, password_hash=password_hash, is_superuser=False)
+    )
+    assert crud.user.is_superuser(user) == False
+
+
+def test_user_is_superuser_superuser(client: TestClient, event_loop: EventLoop):
+    email = random_email()
+    password_hash = random_lower_string()
+    user = event_loop.run_until_complete(
+        User.create(email=email, password_hash=password_hash, is_superuser=True)
+    )
+    assert crud.user.is_superuser(user) == True
--- app/tests/utils/utils.py
+++ app/tests/utils/utils.py
@@ -1,10 +1,31 @@
+from app.core.config import settings
 import random
 import string
+from asyncio import AbstractEventLoop as EventLoop
+from typing import Dict
+from tortoise.models import Model
+from fastapi.testclient import TestClient
 
 
-def random_lower_string() -> str:
-    return "".join(random.choices(string.ascii_lowercase, k=10))
+def random_lower_string(length=20) -> str:
+    return "".join(random.choices(string.ascii_lowercase, k=length))
 
 
-def random_email() -> str:
-    return f"{random_lower_string()}@{random_lower_string()}.com"
+def random_email(length=10) -> str:
+    return f"{random_lower_string(length)}@{random_lower_string(length)}.com"
+
+
+def clean_table(model: Model, event_loop: EventLoop):
+    event_loop.run_until_complete(model.all().delete())
+
+
+def user_authentication_headers(
+    client: TestClient, email: str, password: str
+) -> Dict[str, str]:
+    data = {"username": email, "password": password}
+
+    r = client.post(f"{settings.API_STR}/login/access-token", data=data)
+    response = r.json()
+    auth_token = response["access_token"]
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    return headers
